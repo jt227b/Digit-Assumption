c9804d3348be6f04dec65f64ae985cec7f68e1f5
Jose Torres <jt227b@gmail.com>
2019-09-19 16:07:00 -0500

    Created number guessing game

---
+++ app.js
@@ -0,0 +1,55 @@
+/*
+GAME FUNCTION
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if they lost
+- Let player choose to play again
+*/
+
+// Game Values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'),
+  message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for Guess
+guessBtn.addEventListener('click', function () {
+  let guess = parseInt(guessInput.value);
+
+  // Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter an number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if it's winning number
+  if (guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color to green when you win
+    guessInput.style.borderColor = 'green';
+    // Set message when user wins
+    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+  } else {
+
+  }
+});
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
+
